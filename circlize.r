3d8c9f0ecc320f7b7a727f0c89082cee3fb11bc7
Alvaro Limber Chirino Gutierrez <alvaro.rqsbo@gmail.com>
2014-10-30 14:39:32 -0400

    Update circlize.r

--- circlize.r
+++ circlize.r
@@ -28,29 +28,3 @@ for(i in 1:9){
 }
 dev.off()
 
-circos.clear()
-par(mar = c(1, 1, 1, 1))
-circos.par(cell.padding = c(0, 0, 0, 0),
-              gap.degree = c(2, 2, 10, 2, 2, 2, 2, 2, 10), start.degree = 5)
-circos.initialize(factors = factors, xlim = xlim)
-circos.trackPlotRegion(factors = factors, ylim = c(0, 1), bg.border = NA,
-                          bg.col = c("red", "green", "blue", rep("grey", 6)), track.height = 0.05,
-panel.fun = function(x, y) {
-sector.name = get.cell.meta.data("sector.index")
-          xlim = get.cell.meta.data("xlim")
-          circos.text(mean(xlim), 1.5, sector.name, adj = c(0.5, 0))
-                       1
-                           
-                              # plot white border in the grids
-                              if(sector.name %in% rn) {
-                                for(i in seq_len(ncol(mat))) {
-                                  circos.lines(rep(sum(mat[sector.name, seq_len(i)]), 2), c(0, 1),
-                                                + col = "white")
-                                  }
-                                } else if(sector.name %in% cn) {
-                                  for(i in rev(seq_len(nrow(mat)))) {
-                                    circos.lines(rep(sum(mat[seq_len(i), sector.name]), 2), c(0, 1),
-                                                  + col = "white")
-                                    }
-                                  }
-                            })
